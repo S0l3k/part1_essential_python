3529e1a603e4661c7cd94dfe16477c4318b91196
S0l3k <turtleteamwcg@gmail.com>
2025-09-27 20:39:39 +0300

    срезы и их проявление

--- lists.py
+++ lists.py
@@ -44,34 +44,52 @@ print(some_list)
 # print(min(some_list)) #выводит минимальное занчение по индексу?
 # print(sorted(some_list, key=None, reverse=False)) #сортирует значения в алфавитном порядке
 
-some_list.append('хлеб')
-print(some_list)
-some_list.insert(3, 'бублик')
-print(some_list)
-
-first_hlem_index = some_list.index('хлеб') #говорит какой индекс в списке у нужного элемента, если их несколько, то выводит первый совпавший
-print(f'Элемент "хлеб" был добавлен на позицияю ', first_hlem_index )
-print(some_list.index('хлеб'))
-
-# some_list.remove('хлеб')
+# some_list.append('хлеб')
+# print(some_list)
+# some_list.insert(3, 'бублик')
 # print(some_list)
 
-count = some_list.count('хлеб')
-print(count)
+# first_hlem_index = some_list.index('хлеб') #говорит какой индекс в списке у нужного элемента, если их несколько, то выводит первый совпавший
+# print(f'Элемент "хлеб" был добавлен на позицияю ', first_hlem_index )
+# print(some_list.index('хлеб'))
 
-last_item = some_list.pop() #без указания индекса удаляет последий элемент списка
-print(last_item)
-print(some_list)
-other_item = some_list.pop(3) #должен удалить бублик
-print(other_item)
-print(some_list)
+# # some_list.remove('хлеб')
+# # print(some_list)
 
-# some_list.clear()
+# count = some_list.count('хлеб')
+# print(count)
+
+# last_item = some_list.pop() #без указания индекса удаляет последий элемент списка
+# print(last_item)
+# print(some_list)
+# other_item = some_list.pop(3) #должен удалить бублик
+# print(other_item)
 # print(some_list)
 
-some_list.sort(reverse=True)
-print(some_list) #в чем отличие от sorted()?
+# # some_list.clear()
+# # print(some_list)
 
-some_list_copy = some_list.copy()
-print(some_list_copy.clear())
-print(some_list)
+# some_list.sort(reverse=True)
+# print(some_list) #в чем отличие от sorted()?
+
+# some_list_copy = some_list.copy()
+# print(some_list_copy.clear())
+# print(some_list)
+
+top_slice = some_list[:3]
+print(f'Срез от начала списка до индекса 3, не включая его ', top_slice)
+
+mid_to_end_slice = some_list[1:]
+print(f'Срез от индекса 1, включая его, до конца ', mid_to_end_slice)
+
+middle_slice = some_list[1:3]
+print(f'Срез от 1 до 3 индекса, не включая 3 ', middle_slice)
+
+slice_with_step = some_list[::2]
+print(f'Срез с шагом 2, индексы 0, 2, 4', slice_with_step)
+
+slice_with_step_reverse = some_list[::-1]
+print(f'Срез с конца с шагом 1 ', slice_with_step_reverse)
+
+last_2_hero = some_list[-2:]
+print(f'Срез последних 2 элементов ', last_2_hero)
