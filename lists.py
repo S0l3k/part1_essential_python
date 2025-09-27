7dc815fc07297bd792710d9cb7a2c3ef4a2327a3
S0l3k <turtleteamwcg@gmail.com>
2025-09-27 19:49:36 +0300

    заменил элемент списка

--- lists.py
+++ lists.py
@@ -7,6 +7,9 @@
 some_list = ['колбаса', 'тыква', 'хлеб', 'молоко', 'яйца']
 print(some_list)
 
-print(some_list[0])
-print(some_list[1])
-print(some_list[-1])
+# print(some_list[0])
+# print(some_list[1])
+# print(some_list[-1])
+
+some_list[2] = 'картофель'
+print(some_list)
