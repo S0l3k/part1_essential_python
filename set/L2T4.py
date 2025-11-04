1e3a07a1a14cf818d14581de1e604d26342a771d
S0l3k <turtleteamwcg@gmail.com>
2025-11-04 13:14:19 +0300

    ответ на задачу 4

--- set/L2T4.py
+++ set/L2T4.py
@@ -1,3 +1,12 @@
 # Создайте множество всех студентов класса: {"Anna", "Bob", "Charlie", "Diana"}
 # Создайте множество студентов, сдавших экзамен: {"Anna", "Charlie", "Diana"}
 # Проверьте, все ли студенты сдали экзамен
+
+set_all_students = {"Anna", "Bob", "Charlie", "Diana"}
+set_students_passed_exam = {"Anna", "Charlie", "Diana"}
+students_failed_exam = set_all_students ^ set_students_passed_exam
+
+if len(set_all_students) == len(set_students_passed_exam):
+    print('Все студенты сдали экзамен!')
+else:
+    print('К сожалению, не все студенты сдали экзамен - ', students_failed_exam)
