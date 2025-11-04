02cd26ef48edb8e27b55e63847cfa3e92adf9756
S0l3k <turtleteamwcg@gmail.com>
2025-11-04 13:08:02 +0300

    ответ на задачу 3

--- set/L2T3.py
+++ set/L2T3.py
@@ -2,3 +2,13 @@
 # list1 = ["apple", "banana", "orange", "grape"]
 # list2 = ["banana", "kiwi", "orange", "mango"]
 # Найдите слова, которые встречаются в обоих списках
+
+list1 = ["apple", "banana", "orange", "grape"]
+list2 = ["banana", "kiwi", "orange", "mango"]
+
+set_list1 = set(list1)
+set_list2 = set(list2)
+
+general_values = set_list1 & set_list2
+general_list_values = list(general_values)
+print(general_list_values, ' - слова, которые встречаются в обоих списках')
