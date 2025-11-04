28b9547d7c1e2d55b3bc49cb5097d5f53758af50
S0l3k <turtleteamwcg@gmail.com>
2025-11-04 13:22:12 +0300

    ответ на задачу 5

--- set/L2T5.py
+++ set/L2T5.py
@@ -2,3 +2,14 @@
 # - количество уникальных символов
 # - сами уникальные символы
 # Пример: "hello" → 4 уникальных символа: {'h', 'e', 'l', 'o'}
+
+input_string = 'hello'
+
+def unique_symbol_in_sting(input_str):
+    """Принимает строку, возвращает кол-во уникальных символов и сами символы"""
+
+    str_to_set = set(input_str)
+    len_set = len(str_to_set)
+    return print(f'Кол-во уникльных символов {len_set}, сами уникальные символы {str_to_set}')
+
+unique_symbol_in_sting(input_string)
