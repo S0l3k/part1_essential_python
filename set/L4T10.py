103e12d0012d0dca0bc444df4817e6ea6bf89cf6
S0l3k <turtleteamwcg@gmail.com>
2025-11-08 21:19:31 +0300

    ответ на задачу 10

--- set/L4T10.py
+++ set/L4T10.py
@@ -3,3 +3,22 @@
 # Используйте множества для оптимизации поиска (O(n) вместо O(n²))
 # Пример: numbers = [1, 2, 3, 4, 5, 6], target = 7
 # Результат: [(1, 6), (2, 5), (3, 4)]
+
+numbers = [1, 2, 3, 4, 5, 6]
+target = 7
+
+pairs_are_equal_to_the_target = set()
+list_of_pairs_equal_to_the_target = list()
+
+for first_value in numbers:
+    second_value = target - first_value
+    
+    if second_value in pairs_are_equal_to_the_target:
+        pair = (min(first_value, second_value), max(first_value, second_value))
+        
+        if pair not in list_of_pairs_equal_to_the_target:
+            list_of_pairs_equal_to_the_target.append(pair)
+    
+    pairs_are_equal_to_the_target.add(first_value)
+        
+print(list_of_pairs_equal_to_the_target)
